9d46327fcfcf69b87052d60725909cdd0ef2f29d
jhs512 <jangka512@gmail.com>
2023-03-14 16:31:00 +0900

    불필요한 괄호 제거

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 public class Calc {
     public static int run(String exp) {
+        exp = stripOuterBrackets(exp);
+
         // 단일항이 입력되면 바로 리턴
         if ( !exp.contains(" ") ) return Integer.parseInt(exp);
 
@@ -49,5 +51,13 @@ public class Calc {
         }
 
         throw new RuntimeException("올바른 계산식이 아닙니다.");
+    }
+
+    private static String stripOuterBrackets(String exp) {
+        if ( exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')' ) {
+            exp = exp.substring(1, exp.length() - 1);
+        }
+
+        return exp;
     }
 }
